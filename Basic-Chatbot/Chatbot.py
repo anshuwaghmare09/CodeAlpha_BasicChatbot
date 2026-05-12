6f2d6948706b551c459a095cf4608078078f3465
anshuwaghmare09 <anshulwaghmare13@gmail.com>
2026-05-12 21:00:57 +0530

    Added chatbot code

--- Basic-Chatbot/Chatbot.py
+++ Basic-Chatbot/Chatbot.py
@@ -1,1 +1,21 @@
-print("ChatBot Started")
+def chatBot():
+     
+     print("ChatBot Started")
+     print("Type 'bye' to exit.\n")
+
+     while True:
+          user = input("You:").lower()
+          if user=="hello":
+               print("Bot: Hii")
+          elif user=="how are you":
+               print("Bot: I'm fine, thanks")
+          elif user=="what is your name":
+               print("Bot :I'm simple chatbot.")
+          elif user=="bye":
+               print("Bot: Goodbye")
+               break
+          
+          else:
+               print("Bot: Sorry, I don't understand")
+               
+chatBot()
