7402db51304cad0080cfbd5d0a5bb9e8f0b0dd7c
anshuwaghmare09 <anshulwaghmare13@gmail.com>
2026-05-12 21:18:45 +0530

    Added chatbot code

--- Basic-Chatbot/Chatbot.py
+++ Basic-Chatbot/Chatbot.py
@@ -1,17 +1,31 @@
+import random
 def chatBot():
      
      print("ChatBot Started")
      print("Type 'bye' to exit.\n")
-
+     
+     greetings=["hello","hi","hey"]
+     bye_word = ["bye","byee","exit","quit"]
+     
      while True:
           user = input("You:").lower()
-          if user=="hello":
-               print("Bot: Hii")
+          
+          
+          if user in greetings: 
+               responses=["hi","Hey there","hey","hello"]
+               print("Bot:",random.choice(responses))
+                    
+                    
           elif user=="how are you":
-               print("Bot: I'm fine, thanks")
+               responses=["I'm fine","Doing great","All good"]
+               print("Bot:",random.choice(responses))
+               
+               
           elif user=="what is your name":
-               print("Bot :I'm simple chatbot.")
-          elif user=="bye":
+               print("Bot :I'm a simple chatbot.")
+          
+          
+          elif user in bye_word:
                print("Bot: Goodbye")
                break
           
